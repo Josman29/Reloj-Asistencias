aed38b619e21e4229769dc9aabf093bd55d6d6d6
Rodolfo Medina <49995661+rodomedina@users.noreply.github.com>
2022-09-08 19:56:14 -0300

    Se agregan comentarios a clases
    clsconexion
    clsUSR

--- Datos/clsConexion.vb
+++ Datos/clsConexion.vb
@@ -1,28 +1,38 @@
 ﻿Imports System.Data.SqlClient
 
 Public Class clsConexion
+    'Creamos un objeto del tipo SqlConnection
     Public con As New SqlConnection
-
+    'Funcion Conectado nos permitira realizar una conexion a un servidor y base especificado en el objeto con
     Public Function Conectado()
         Try
+            'Agregamos al objeto las credenciales del servidor sql
             con = New SqlConnection("data source=DESKTOP-D5FL463\SQLEXPRESS;initial catalog=Sistema; Integrated Security = True")
+            'Abrimos la conexion
             con.Open()
+            'Retornamos el valor true para informar el exito de la conexion
             Return True
         Catch ex As Exception
+            'Si sucede algun error, informamos por mensaje de pantalla el detalle
             MsgBox(ex.Message)
             Return False
         End Try
     End Function
-
+    'Una vez que tenemos resultados de la base siempre deberemos cerrar la conexion para no ocupar recursos sin sentido del servidor sql
     Public Function Desconectado()
         Try
+            'Preguntamos si la conexion esta abierta
             If con.State = ConnectionState.Open Then
+                'Si esto es asi, cerramos la conexion
                 con.Close()
+                'Devolvemos el valor true para informar que la conexion se encuentra cerrada
                 Return True
             Else
-                Return False
+                'Devolvemos el valor true para informar que la conexion se encuentra cerrada
+                Return True
             End If
         Catch ex As Exception
+            'Si sucede algun error, informamos por mensaje de pantalla el detalle
             MsgBox(ex.Message)
             Return False
         End Try
--- Datos/clsUSR.vb
+++ Datos/clsUSR.vb
@@ -1,25 +1,35 @@
-﻿Imports System.Data.SqlClient
+﻿'Importamos las funciones sqlClient
+Imports System.Data.SqlClient
 Public Class clsUSR
-
+    'Heredamos la clase conexion para utilizar sus metodos
     Inherits clsConexion
+    'Creamos un sqlCommand para conectarnos a los procedimientos de sql
     Dim cmd As New SqlCommand
 
+    'Funcion GetLogin nos permite realizar el proceso de validacion de usuario
     Public Function GetLogin(usu As String, pas As String) As DataTable
         With cmd
+            'Utilizamos el metodo de la clase clsConexion para conectarnos
             Conectado()
+            'Establecemos el procedidmiento que vamos a ejecutar
             cmd = New SqlCommand("SP_USU_GET_LOG")
+            'Establecemos el tipo de comando que vamos a ejecutar, en este caso procedimiento almacenado
             cmd.CommandType = CommandType.StoredProcedure
+            'Agreamos los valores de los parametros del procedimiento, en este caso dos 
             cmd.Parameters.AddWithValue("@USR_NIC", usu)
             cmd.Parameters.AddWithValue("@USR_PAS", pas)
+            'Agregamos la cadena de conexion
             cmd.Connection = con
-            cmd.CommandTimeout = 999999999
-
-
+            'Establecemos el tiempo maximo de espera a respuesta de sql
+            cmd.CommandTimeout = 9999
+            'Creamos una variable del tipo DataTable para almacenar el resultado
             Dim dt As New DataTable
+            'Creamos una variable dataAdpater para ajustar el resultado al DataTable
             Dim da As New SqlDataAdapter(cmd)
+            'Se agrega el resultado del procedimiento al DataTable
             da.Fill(dt)
+            'Como paso final de la funcion devolvemos el resultado
             Return dt
-
         End With
     End Function
 
